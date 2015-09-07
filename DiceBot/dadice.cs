846af93ea7537b22ebd052ce78a39ed3aa8183d6
seuntie900 <seuntjie900@gmail.com>
2015-09-07 14:28:47 +0200

    Migrated dadice to httpclient
    minor bug fixes for dadice and added donate method

--- DiceBot/dadice.cs
+++ DiceBot/dadice.cs
@@ -7,6 +7,7 @@ using System.Threading;
 using System.Net;
 using System.IO;
 using System.Security.Cryptography;
+using System.Net.Http;
 
 namespace DiceBot
 {
@@ -29,8 +30,9 @@ namespace DiceBot
             Tip = true;
             TipUsingName = true;
             SiteURL = "https://www.dadice.com/?referrer=seuntjie";
+            
         }
-
+        HttpClient Client = new HttpClient { BaseAddress = new Uri("https://dadice.com/api/") };
         int betcount = 0;
         DateTime Lastbet = DateTime.Now;
         void PlaceBetThread(object _High)
@@ -39,21 +41,20 @@ namespace DiceBot
             {
                 Lastbet = DateTime.Now;
                 bool High = (bool)_High;
-                HttpWebRequest betrequest = (HttpWebRequest)HttpWebRequest.Create("https://dadice.com/api/roll");
-                betrequest.Method = "POST";
-                string post = string.Format(System.Globalization.NumberFormatInfo.InvariantInfo, "username={0}&key={1}&amount={2:0.00000000}&chance={3:00.00}&bet={4}", username, key, amount, chance, High ? "over" : "under");
-                betrequest.ContentLength = post.Length;
-                if (Prox != null)
-                    betrequest.Proxy = Prox;
-                betrequest.ContentType = "application/x-www-form-urlencoded; charset=UTF-8";
-                using (var writer = new StreamWriter(betrequest.GetRequestStream()))
-                {
+                List<KeyValuePair<string, string>> pairs = new List<KeyValuePair<string, string>>();
+                pairs.Add(new KeyValuePair<string, string>("username",username));
+                pairs.Add(new KeyValuePair<string, string>("key", key));
+                pairs.Add(new KeyValuePair<string, string>("amount", amount.ToString()));
+                pairs.Add(new KeyValuePair<string, string>("chance", chance.ToString()));
+                pairs.Add(new KeyValuePair<string, string>("bet", High ? "over" : "under"));
 
-                    writer.Write(post);
+                FormUrlEncodedContent Content = new FormUrlEncodedContent(pairs);
+                DADICERollBase tmp = new DADICERollBase();
+                using (var response = Client.PostAsync("roll", Content))
+                {
+                    string sEmitResponse2 = response.Result.Content.ReadAsStringAsync().Result;
+                    tmp = json.JsonDeserialize<DADICERollBase>(sEmitResponse2);
                 }
-                HttpWebResponse EmitResponse2 = (HttpWebResponse)betrequest.GetResponse();
-                string sEmitResponse2 = new StreamReader(EmitResponse2.GetResponseStream()).ReadToEnd();
-                DADICERollBase tmp = json.JsonDeserialize<DADICERollBase>(sEmitResponse2);
                 if (tmp.status)
                 {
 
@@ -112,21 +113,20 @@ namespace DiceBot
         {
             try
             {
-                HttpWebRequest betrequest = (HttpWebRequest)HttpWebRequest.Create("https://dadice.com/api/withdraw");
-                betrequest.Method = "POST";
-                string post = string.Format(System.Globalization.NumberFormatInfo.InvariantInfo, "username={0}&key={1}&coin=btc&payee={2}&amount={3:0.00000000}", username, key, Address, Amount);
-                betrequest.ContentLength = post.Length;
-                if (Prox != null)
-                    betrequest.Proxy = Prox;
-                betrequest.ContentType = "application/x-www-form-urlencoded; charset=UTF-8";
-                using (var writer = new StreamWriter(betrequest.GetRequestStream()))
-                {
+                List<KeyValuePair<string, string>> pairs = new List<KeyValuePair<string, string>>();
+                pairs.Add(new KeyValuePair<string, string>("username", username));
+                pairs.Add(new KeyValuePair<string, string>("key", key));
+                pairs.Add(new KeyValuePair<string, string>("amount", Amount.ToString()));
+                pairs.Add(new KeyValuePair<string, string>("payee", Address));                
 
-                    writer.Write(post);
+                FormUrlEncodedContent Content = new FormUrlEncodedContent(pairs);
+                DADICEBlance tmp = new DADICEBlance();
+                using (var response = Client.PostAsync("withdraw", Content))
+                {
+                    string sEmitResponse2 = response.Result.Content.ReadAsStringAsync().Result;
+                    tmp = json.JsonDeserialize<DADICEBlance>(sEmitResponse2);
                 }
-                HttpWebResponse EmitResponse2 = (HttpWebResponse)betrequest.GetResponse();
-                string sEmitResponse2 = new StreamReader(EmitResponse2.GetResponseStream()).ReadToEnd();
-                DADICEBlance tmp = json.JsonDeserialize<DADICEBlance>(sEmitResponse2);
+                LastBalance = DateTime.Now.AddMinutes(-1);
                 return tmp.status;
             }
             catch (Exception E)
@@ -141,44 +141,33 @@ namespace DiceBot
         {
             username = Username;
             key = Password;
-
-            HttpWebRequest betrequest = (HttpWebRequest)HttpWebRequest.Create("https://dadice.com/api/balance");
-            betrequest.Method = "POST";
-            string post = string.Format(System.Globalization.NumberFormatInfo.InvariantInfo, "username={0}&key={1}", username, key);
-            betrequest.ContentLength = post.Length;
-            if (Prox != null)
-                betrequest.Proxy = Prox;
-            betrequest.ContentType = "application/x-www-form-urlencoded; charset=UTF-8";
-            using (var writer = new StreamWriter(betrequest.GetRequestStream()))
+            List<KeyValuePair<string, string>> pairs = new List<KeyValuePair<string, string>>();
+            pairs.Add(new KeyValuePair<string, string>("username", username));
+            pairs.Add(new KeyValuePair<string, string>("key", key));
+            FormUrlEncodedContent Content = new FormUrlEncodedContent(pairs);
+            DADICEBlance tmp = new DADICEBlance();
+            using (var response = Client.PostAsync("balance", Content))
             {
-
-                writer.Write(post);
-            }
-            HttpWebResponse EmitResponse2 = (HttpWebResponse)betrequest.GetResponse();
-            string sEmitResponse2 = new StreamReader(EmitResponse2.GetResponseStream()).ReadToEnd();
-            DADICEBlance tmp = json.JsonDeserialize<DADICEBlance>(sEmitResponse2);
+                string Resp = response.Result.Content.ReadAsStringAsync().Result;
+                tmp = json.JsonDeserialize<DADICEBlance>(Resp);
+            }            
             if (tmp.status)
             {
-                HttpWebRequest betrequest2 = (HttpWebRequest)HttpWebRequest.Create("https://dadice.com/api/deposit");
-                betrequest2.Method = "POST";
-                string post2 = string.Format(System.Globalization.NumberFormatInfo.InvariantInfo, "username={0}&key={1}&coin=btc", username, key);
-                betrequest2.ContentLength = post.Length;
-                if (Prox != null)
-                    betrequest.Proxy = Prox;
-                betrequest2.ContentType = "application/x-www-form-urlencoded; charset=UTF-8";
-                using (var writer = new StreamWriter(betrequest2.GetRequestStream()))
+                pairs.Add(new KeyValuePair<string, string>("coin", "btc"));
+                Content = new FormUrlEncodedContent(pairs);
+                DADICEBlance dep = new DADICEBlance();
+                using (var response = Client.PostAsync("deposit", Content))
                 {
-
-                    writer.Write(post);
+                    string Resp = response.Result.Content.ReadAsStringAsync().Result;
+                    dep = json.JsonDeserialize<DADICEBlance>(Resp);
                 }
-                HttpWebResponse EmitResponse3 = (HttpWebResponse)betrequest2.GetResponse();
-                string sEmitResponse3 = new StreamReader(EmitResponse3.GetResponseStream()).ReadToEnd();
-                DADICEBlance dep = json.JsonDeserialize<DADICEBlance>(sEmitResponse3);
+                
                 if (dep.status)
                     Parent.updateDeposit(dep.address);
-
-
+                
                 balance = (double)tmp.balance;
+
+                
                 HttpWebRequest loginrequest = (HttpWebRequest)HttpWebRequest.Create("https://stats.dadice.com/api/userinfo?username=" + Username);
                 if (Prox != null)
                     loginrequest.Proxy = Prox;
@@ -234,22 +223,16 @@ namespace DiceBot
                     try
                     {
 
-
-                        HttpWebRequest betrequest = (HttpWebRequest)HttpWebRequest.Create("https://dadice.com/api/balance");
-                        betrequest.Method = "POST";
-                        string post = string.Format(System.Globalization.NumberFormatInfo.InvariantInfo, "username={0}&key={1}", username, key);
-                        betrequest.ContentLength = post.Length;
-                        if (Prox != null)
-                            betrequest.Proxy = Prox;
-                        betrequest.ContentType = "application/x-www-form-urlencoded; charset=UTF-8";
-                        using (var writer = new StreamWriter(betrequest.GetRequestStream()))
+                        List<KeyValuePair<string, string>> pairs = new List<KeyValuePair<string, string>>();
+                        pairs.Add(new KeyValuePair<string, string>("username", username));
+                        pairs.Add(new KeyValuePair<string, string>("key", key));
+                        FormUrlEncodedContent Content = new FormUrlEncodedContent(pairs);
+                        DADICEBlance tmp = new DADICEBlance();
+                        using (var response = Client.PostAsync("balance", Content))
                         {
-
-                            writer.Write(post);
+                            string Resp = response.Result.Content.ReadAsStringAsync().Result;
+                            tmp = json.JsonDeserialize<DADICEBlance>(Resp);
                         }
-                        HttpWebResponse EmitResponse2 = (HttpWebResponse)betrequest.GetResponse();
-                        string sEmitResponse2 = new StreamReader(EmitResponse2.GetResponseStream()).ReadToEnd();
-                        DADICEBlance tmp = json.JsonDeserialize<DADICEBlance>(sEmitResponse2);
                         if (tmp.status)
                             balance = (double)tmp.balance;
                         Parent.updateBalance(balance);
@@ -285,25 +268,26 @@ namespace DiceBot
         {
             Parent.updateStatus("Can't chat at this time. Sorry!");
         }
-
+        public override void Donate(double Amount)
+        {
+            SendTip("seuntjie", Amount);
+        }
         void SendTipThread(object args)
         {
             try
             {
-                HttpWebRequest betrequest = (HttpWebRequest)HttpWebRequest.Create("https://dadice.com/api/tip");
-                betrequest.Method = "POST";
-                string post = string.Format(System.Globalization.NumberFormatInfo.InvariantInfo, "username={0}&key={1}&payee={2}&amount={3:0.00000000}", username, key, (args as string[])[0], (args as string[])[1]);
-                betrequest.ContentLength = post.Length;
-                if (Prox != null)
-                    betrequest.Proxy = Prox;
-                betrequest.ContentType = "application/x-www-form-urlencoded; charset=UTF-8";
-                using (var writer = new StreamWriter(betrequest.GetRequestStream()))
+                List<KeyValuePair<string, string>> pairs = new List<KeyValuePair<string, string>>();
+                pairs.Add(new KeyValuePair<string, string>("username", username));
+                pairs.Add(new KeyValuePair<string, string>("key", key));
+                pairs.Add(new KeyValuePair<string, string>("payee", (args as string[])[0]));
+                pairs.Add(new KeyValuePair<string, string>("amount", (args as string[])[1]));
+                FormUrlEncodedContent Content = new FormUrlEncodedContent(pairs);
+                
+                using (var response = Client.PostAsync("tip", Content))
                 {
-
-                    writer.Write(post);
+                    string Resp = response.Result.Content.ReadAsStringAsync().Result;
+                    
                 }
-                HttpWebResponse EmitResponse2 = (HttpWebResponse)betrequest.GetResponse();
-                string sEmitResponse2 = new StreamReader(EmitResponse2.GetResponseStream()).ReadToEnd();
             }
             catch (Exception E)
             {
