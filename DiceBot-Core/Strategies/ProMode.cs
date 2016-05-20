ba19ecc0311381241dcd4da16641a42ea3f7b6a9
Seuntjie900 <seuntjie900@gmail.com>
2016-05-20 16:55:55 +0200

    Started implementing promode

---
+++ DiceBot-Core/Strategies/ProMode.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceBotCore.Strategies
+{
+    class ProMode:StrategyBase
+    {
+        public double BaseBet { get; set; }
+        public double StartingChance { get; set; }
+
+        public override double CalculateNextBet(double Lastbet, bool Win)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double RunReset()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ProModeControl
+    {
+        public List<ProModeCondition> Conditions { get; set; }
+        public List<ProModeAction> Actions { get; set; }
+
+    }
+
+    public class ProModeCondition
+    {
+        public string FirstVar { get; set; }
+        public string SecondVar { get; set; }
+        public ProModeLogic LogicOperator { get; set; }
+        
+    }
+    public class ProModeAction
+    {
+        public ProModeOperators Operator { get; set; }
+        public string FirstVar { get; set; }
+        public string SecondVar { get; set; }
+    }
+    public enum ProModeLogic
+    {
+        equals,
+        not_equals,
+        larger_than,
+        Smaller_than,
+        larger_or_equals_to,
+        smaller_or_equals_to
+    }
+    public enum ProModeOperators
+    {
+        plus,
+        minus,
+        times_with,
+        devide_by,
+        modulus,
+        percentage_of
+
+    }
+    public enum ProModeLogicExtentions
+    {
+        and,
+        or,
+        xor
+        
+    }
+}
