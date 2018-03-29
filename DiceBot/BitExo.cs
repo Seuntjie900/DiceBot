ebf3e6d3d4c222838f81660fb91d0c9fcc3a88f7
Seuntjie900 <seuntjie900@gmail.com>
2018-03-29 17:46:41 +0200

    Added flash to bit-exo

--- DiceBot/BitExo.cs
+++ DiceBot/BitExo.cs
@@ -13,7 +13,7 @@ namespace DiceBot
     class BitExo:DiceSeuntjie
     {
 
-        new public static string[] sCurrencies = new string[] { "BTC","LTC","DASH","ADK","GRLC" };
+        new public static string[] sCurrencies = new string[] { "BTC","LTC","DASH","ADK","GRLC","FLASH" };
 
         public BitExo(cDiceBot Parent):base(Parent)
         {
