9ffc081a68df753059a1e86e9da428c2da213475
Seuntjie900 <seuntjie900@gmail.com>
2019-05-21 19:18:49 +0200

    Version update

--- DiceBot/cDiceBot.cs
+++ DiceBot/cDiceBot.cs
@@ -33,7 +33,7 @@ namespace DiceBot
         #endregion
 
         //Version number to test against site
-        private const string vers = "3.3.25";
+        private const string vers = "3.3.26";
         
 
         Control[] ControlsToDisable;
