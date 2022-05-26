e22bed6820b18cd280f538b914a7a6bc143b1d22
WinMachine777 <winmachine1982@gmail.com>
2022-05-26 14:07:37 -0700

    fix stake bet

--- DiceBot/PD.cs
+++ DiceBot/PD.cs
@@ -87,7 +87,7 @@ namespace DiceBot
                                 this.bets = (int)x.bets;
                                 this.wins = (int)x.wins;
                                 this.losses = (int)x.losses;
-                                this.profit = (decimal)x.profit;
+                                this.profit = x.profit.HasValue ? (decimal)x.profit.Value : 0;
                                 this.wagered = (decimal)x.betAmount;
                                 break;
                             }
@@ -309,7 +309,7 @@ namespace DiceBot
                                 this.bets = (int)x.bets;
                                 this.wins = (int)x.wins;
                                 this.losses = (int)x.losses;
-                                this.profit = (decimal)x.profit;
+                                this.profit = x.profit.HasValue ? (decimal)x.profit.Value : 0;
                                 this.wagered = (decimal)x.betAmount;
                                 break;
                             }
